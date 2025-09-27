72bef7e132c2bcc811d1ddcfd3649d94aad2fc70
Froshlin <onitimilehin87@gmail>
2025-09-27 09:23:21 +0100

    Modified HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -25,7 +25,7 @@ function HeroSection() {
 
         <div className="flex flex-col sm:flex-row gap-4 justify-center lg:justify-start">
           <a
-            href="projects"
+            href="#projects"
             className="bg-blue-600 text-white px-6 py-3 rounded-lg hover:bg-blue-700 transition-colors duration-300 text-center font-medium"
           >
             View Projects
